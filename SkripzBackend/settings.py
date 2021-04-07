e6defd5912e8ec2204199850409436d04af44cf1
Arnastria <arnas.bgr@gmail.com>
2021-04-07 14:02:32 +0700

    add heroku settings

--- SkripzBackend/settings.py
+++ SkripzBackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '3^an3hf7s*bxwqm6)ha0c54+-rp407ekv1maaa#xw(w8ql94d2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['skripz-backend.herokuapp.com', ]
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,6 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
